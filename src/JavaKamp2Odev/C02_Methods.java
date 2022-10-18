7e7b9349652c6ec7fcccd7fdf5cef33bc585401f
passenger3e <ali_vara_01@hotmail.com>
2022-10-18 20:49:16 +0300

    Initial Commit

---
+++ src/JavaKamp2Odev/C02_Methods.java
@@ -0,0 +1,12 @@
+package JavaKamp2Odev;
+
+public class C02_Methods {
+    public static void main(String[] args) {
+        int number=101;
+        String message="Number is: "+number;
+    giveMessage(message);
+    }
+    public static void giveMessage(String _message){
+        System.out.println(_message);
+    }
+}
